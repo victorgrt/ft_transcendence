443789b328dfe3533b10b10bce41b91b0b9f4075
Victor GORET <vgoret@made-f0Br4s12.clusters.42paris.fr>
2024-06-10 17:45:20 +0200

    added norme for me

---
+++ srcs/tmp/norme.cpp
@@ -0,0 +1,8 @@
+/*################################//
+whatdoing : returns nothing
+to_do : implement this shit
+//################################*/
+void ft_name(void)
+{
+	return ;
+}
---
+++ srcs/tmp/norme.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+/*############INCLUDES############*/
+#include <unistd.h>
+
+/*############ CLASS ############*/
+class nothing{};
+
+/*###########       #############//
+ft_name : returns nothing
+args : none
+to_do : implement this shit
+//################################*/
+void ft_name(void);
