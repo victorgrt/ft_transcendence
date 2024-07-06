59a4f7c32afc33323a04dfafb708b88b3c3a9884
theo <theo.eliet@gmail.com>
2024-07-06 20:09:17 +0200

    fix create game socket connection

--- srcs/app_django/pages/game_manager.py
+++ srcs/app_django/pages/game_manager.py
@@ -5,12 +5,13 @@ from asgiref.sync import async_to_sync
 
 class Game:
     def __init__(self, game_id):
-        self.game_id = f"game_{game_id}"  # Ensure the game_id is a valid string
+        self.game_id = game_id  # Ensure the game_id is a valid string
         self.players = []
         self.ball_position = [0, 0]
         self.ball_velocity = [1, 1]
         self.player_1_position = [0]
         self.game_over = False
+        self.state = "waiting"  # waiting, playing, game_over
 
     def add_player(self, player):
         self.players.append(player)
@@ -19,22 +20,31 @@ class Game:
         self.players.remove(player)
 
     def update(self):
-        # Update game state, e.g., move ball, check collisions
-        self.ball_position[0] += self.ball_velocity[0]
-        self.ball_position[1] += self.ball_velocity[1]
-        # Add more game logic here
-        self.player_1_position[0] += 1;
-        # Send the updated game state to the WebSocket group
+        
+        if self.state == "playing":
+          # Update game state, e.g., move ball, check collisions
+          self.ball_position[0] += self.ball_velocity[0]
+          self.ball_position[1] += self.ball_velocity[1]
+          # Add more game logic here
+          self.player_1_position[0] += 1
+          # Send the updated game state to the WebSocket group
+
+        if self.state == "waiting":
+            # If there are enough players, start the game
+            if len(self.players) >= 2:
+              self.state = "playing"
+
         self.send_game_state()
 
     def send_game_state(self):
-        # print("Sending game state to group %s" % self.game_id)
+        print("Sending game state to group %s" % self.game_id)
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             self.game_id,
             {
                 'type': 'game_update',
                 'message': {
+                    'state': self.state,
                     'ball_position': self.ball_position,
                     'player_1_position': self.player_1_position,
                     # Add more game state information as needed
@@ -47,9 +57,9 @@ class GameManager:
         self.games = {}
         self.lock = threading.Lock()
 
-    def create_game(self):
+    def create_game(self, game_id):
+        # Here, check that the game_id is unique
         print("Creating game n %d" % (len(self.games) + 1))
-        game_id = len(self.games) + 1
         game = Game(game_id)
         self.games[game_id] = game
         return game
@@ -66,7 +76,8 @@ class GameManager:
             with self.lock:
                 for game in self.games.values():
                     game.update()
-            time.sleep(0.016)  # 60 FPS
+            # time.sleep(0.016)  # 60 FPS
+            time.sleep(1)
 
 game_manager = GameManager()
 game_update_thread = threading.Thread(target=game_manager.update_games)
