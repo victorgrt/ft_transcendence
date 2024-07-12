ea974edbe198588fc43ff21d10410984bddb4c7b
Lucas BOUGUET <lbouguet@paul-f5Ar1s2.clusters.42paris.fr>
2024-07-12 15:28:26 +0200

    MatchHistory model + add match record function

--- srcs/app_django/game/models.py
+++ srcs/app_django/game/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
-# class	MatchHistory(models.Model):	
-# 	winner = models.CharFieldmax(max_length=255)	
-# 	loser = models.CharFieldmax(max_lenght=255)	
-# 	date = models.DateTimeField(auto_now_add=True)
+class MatchHistory(models.Model):
+    winner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='won_matches',
+        on_delete=models.CASCADE
+    )
+    loser = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='lost_matches',
+        on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(auto_now_add=True)
 	
--- srcs/app_django/game/views.py
+++ srcs/app_django/game/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import MatchHistory
+from account.models import CustomUser
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+# Add record to the MatchHistory model. 
+def finished_match(request, winner_id, loser_id):
+	user_winner = get_user_model().objects.get(id=winner_id)
+	user_loser = get_user_model().objects.get(id=loser_id)
+	new_match = MatchHistory.objects.create(winner=user_winner, loser=user_loser)
