3dc182b6b0d8968a0be6930c9238936946a69d3d
theo <theo.eliet@gmail.com>
2024-07-17 18:47:03 +0200

    authentify user at socket connection

--- srcs/app_django/game/consumers.py
+++ srcs/app_django/game/consumers.py
@@ -1,11 +1,66 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+django.setup()
+
+# Now we can safely import and use Django models and other components
 import json
 import datetime
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.contrib.sessions.models import Session
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+
 from .game_manager import game_manager
+
+# Wrap the synchronous Django ORM calls with database_sync_to_async
+
+@database_sync_to_async
+def get_session(session_key):
+    return Session.objects.get(session_key=session_key)
+
+@database_sync_to_async
+def get_user(uid):
+    return get_user_model().objects.get(pk=uid)
+
+# Function to get the user object from the session key
+async def get_user_from_session_key(session_key):
+    try:
+        session = await get_session(session_key)
+        uid = session.get_decoded().get('_auth_user_id')
+        user = await get_user(uid)
+        return user
+    except ObjectDoesNotExist:
+        return None
 
 class PongConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
+        
+        # Authenticate the user
+        cookies_header = next((value for (key, value) in self.scope['headers'] if key == b'cookie'), None)
+        user = None  # Initialize user as None
+        if cookies_header:
+            cookies = dict(cookie.split(b"=") for cookie in cookies_header.split(b"; "))
+            session_key = cookies.get(b'sessionid')
+            if session_key:
+                session_key = session_key.decode('utf-8')  # Ensure session_key is a string
+                user = await get_user_from_session_key(session_key)
+                print(user)
+            else:
+                print("Session key not found in cookies")
+        else:
+            print("No cookies found")
+
+        # If user is not found, reject the connection
+        if not user:
+            print("Authentication failed. Closing connection.")
+            await self.close()
+            return  # Stop further execution
+        
+        # Extract the game_id 
         game_id = self.scope['url_route']['kwargs']['game_id']
         self.game_id = game_id
         print(f"Connecting to game {self.game_id}")
