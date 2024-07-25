825e84d73dbd9c80a880fc9464663277a67d8ad1
Lucas BOUGUET <lbouguet@made-f0Br6s16.clusters.42paris.fr>
2024-07-25 16:01:38 +0200

    send_notification_service fixed

--- srcs/app_django/notification/urls.py
+++ srcs/app_django/notification/urls.py
@@ -1,6 +1,7 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
+from .services.notification_services import send_notification_service
 
-# urlpatterns = [
-#     path('', send_notification_service, name='send_ta_mere')
-# ]
+urlpatterns = [
+    path('', send_notification_service, name='send_notification_service')
+]
