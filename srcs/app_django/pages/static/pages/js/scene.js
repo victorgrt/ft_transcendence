a69b25ea97a9cd9048c25b6168665a08c1c0c9ec
victorgrt <victor.goret@laposte.net>
2024-07-26 20:58:32 +0200

    better lights + lamp switching

--- srcs/app_django/pages/static/pages/js/scene.js
+++ srcs/app_django/pages/static/pages/js/scene.js
@@ -8,6 +8,7 @@ var selecting_clickable;
 
 var initialControlPosition;
 var clickCoordinates = null;
+let lampOn = true;
 function init() {
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -29,7 +30,7 @@ function init() {
     scene.add(light);
 
     loader = new THREE.GLTFLoader();
-    const sceneurl = "/staticfiles/pages/images/please.gltf";
+    const sceneurl = "/staticfiles/pages/images/scene_light2.gltf";
 
     loader.load(
         sceneurl,
@@ -50,15 +51,9 @@ function init() {
                             child.intensity = 0.5; // Exemple: Réduire l'intensité d'une lumière ponctuelle
                         }
                         if (child.name === 'PointLight')
-                        {
-                            child.intensity = 15;
-                            console.log(child.name, " booste a ", child.intensity);
-                        }
-                        if (child.name === 'Point002' || child.name === 'Point003')
-                        {
                             child.intensity = 5;
-                            console.log(child.name, " booste a ", child.intensity);
-                        }
+                        if (child.name === 'Point001' || child.name === 'Point')
+                            child.intensity = 1.5;
                         // if (child instanceof THREE.AmbientLight) {
                             // Si l'objet est une lumière ambiante, ajustez ses propriétés
                             // child.intensity = 0; // Exemple: Réduire l'intensité de la lumière ambiante
@@ -104,6 +99,13 @@ function onMouseMove(event) {
     var intersects = raycaster.intersectObjects(scene.children, true);
     // Réinitialiser l'objet surligné précédent
     if (highlightedObject) {
+        if (highlightedObject.name === "lampSquareFloor_2")
+        {
+            // 0.7454042095350284, g: 0.010960094003125918, b: 0.01764195448412081 }
+            highlightedObject.material.color.setRGB(0.7454042095350284, 0.010960094003125918, 0.01764195448412081);
+            highlightedObject = null;
+            return;
+        }
         highlightedObject.material.emissiveIntensity = 1; // Réinitialiser l'intensité d'émission
         highlightedObject = null;
     }
@@ -112,14 +114,15 @@ function onMouseMove(event) {
         // document.body.style.cursor = 'pointer';
         var selectedObject = intersects.find(function (intersect) {
             // Check si l'utilisteur est sur un objet cliquable
-            if ((intersect.object.name === 'GameScreen_Plane' || intersect.object.name === 'computerScreen_2_1' || intersect.object.name === 'mesh_130') && isZooming === false) {
+            if ((intersect.object.name === 'GameScreen_Plane' || intersect.object.name === 'computerScreen_2_1' || intersect.object.name === 'lampSquareFloor_2') && isZooming === false) {
+                console.log("HERE:", intersect.object);
                 selected_object_name = intersect.object.name;
                 return intersect.object.name;
             }
         });
 
         if (selectedObject) {
-            console.log("got here");
+            document.body.style.cursor = 'pointer';
             selecting_clickable = true;
             var objectToHighlight = selectedObject.object;
             //ARCADE MACHINE
@@ -129,15 +132,16 @@ function onMouseMove(event) {
             else if (selectedObject.object.name === 'computerScreen_2_1') {
                 objectToHighlight.material.emissiveIntensity = 5; // Exemple: intensité d'émission pour la surbrillance
             }
-            else if (selectedObject.object.name === 'mesh_130') {
-                objectToHighlight.material.emissiveIntensity = 100; // Exemple: intensité d'émission pour la surbrillance
+            else if (selectedObject.object.name === 'lampSquareFloor_2') {
+                // objectToHighlight.material.emissiveIntensity = 100; // Exemple: intensité d'émission pour la surbrillance
+                objectToHighlight.material.color.setHex(0xFFFFFF);
             }
             // Autres ajustements de surbrillance si nécessaire
             highlightedObject = objectToHighlight;
         }
         else if (!selectedObject) {
             selecting_clickable = false;
-            // document.body.style.cursor = 'default';
+            document.body.style.cursor = 'default';
         }
     }
 }
@@ -190,6 +194,23 @@ function onClickScene(event) {
 
     if (intersects.length > 0) {
         const intersection = intersects[0];
+        // console.log("here:", intersection);
+        if (intersection.object.name === "lampSquareFloor_2")
+        {
+            console.log("clicked on lamp :", intersection);
+            let lampLight = scene.getObjectByName("PointLight");
+            if (lampOn === true)
+            {
+                lampLight.intensity = 0.5;
+                lampOn = false;
+            }
+            else
+            {
+                lampLight.intensity = 5;
+                lampOn = true;
+            }
+            return ;
+        }
         clickCoordinates = intersection.point;
         zoomToCoordinates(clickCoordinates);
     }
