8fb8ca7b55addc4535221c3be51390eff6e5128b
Victor GORET <vgoret@bess-f2r3s7.clusters.42paris.fr>
2024-07-31 19:35:39 +0200

    added badgateway

--- srcs/app_django/pages/views.py
+++ srcs/app_django/pages/views.py
@@ -184,3 +184,6 @@ def accept_friend_request(request):
             return JsonResponse({'status': 'error', 'message': str(e)})
 
     return JsonResponse({'status': 'error', 'message': 'Invalid request.'})
+
+def badGateway(request):
+    return render(request, 'partials/badGateway.html')
